2bfeaaeac94aedd33ab0d82c2dff6679b7471528
burret <32988772+burret@users.noreply.github.com>
2021-12-21 22:02:00 -0800

    Initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Bill Buchanan: FFX schemes for Format-Preserving Encryption
+  https://medium.com/asecuritysite-when-bob-met-alice/ffx-schemes-for-format-preserving-encryption-a2a7aa4f1377
+  https://asecuritysite.com/encryption/fpe
+
+Johannes Dollinger: pyffx is a pure Python implementation of Format-preserving, Feistel-based encryption (FFX).
+  https://github.com/emulbreh/pyffx
---
+++ ffx.py
@@ -0,0 +1,31 @@
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from textwrap import dedent
+
+import pyffx
+
+DESCRIPTION = '''\
+    Format-preserving, Feistel-based cryptography.
+        $ echo 4012888888881881 | python %(prog)s -e qwerty 0123456789
+        9356030022219797
+        $ echo 9356030022219797 | python %(prog)s -d qwerty 0123456789
+        4012888888881881
+        $ echo '$😊qwerty🍦*' | python %(prog)s -e secret-squirrel '*$😊🍦abcdefghijklmnopqrstuvwxyz'
+        rgmty🍦udpv
+        $ echo 'rgmty🍦udpv' | python %(prog)s -d secret-squirrel '*$😊🍦abcdefghijklmnopqrstuvwxyz'
+        $😊qwerty🍦*
+'''
+
+parser = ArgumentParser(formatter_class=RawDescriptionHelpFormatter, description=dedent(DESCRIPTION))
+verb_group = parser.add_mutually_exclusive_group(required=True)
+verb_group.add_argument('-e', action='store_true', help='Encode.')
+verb_group.add_argument('-d', action='store_true', help='Decode.')
+parser.add_argument('key', help='Passphrase.')
+parser.add_argument('alphabet', help='Symbol set.')
+args = parser.parse_args()
+
+key = bytearray(args.key, encoding='ascii', errors='strict')
+input_ = input()
+ffx = pyffx.String(key, alphabet=args.alphabet, length=len(input_))
+process = ffx.encrypt if args.e else ffx.decrypt
+output = process(input_)
+print(output)
